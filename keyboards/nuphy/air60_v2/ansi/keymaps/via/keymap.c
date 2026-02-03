c10667b00020b8993248d66ff954c5c81e0f10cc
Ros McMahon <rosmcmahon@gmail.com>
2026-02-03 11:56:29 +0000

    task: rcmd => fn key

--- keyboards/nuphy/air60_v2/ansi/keymaps/via/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/via/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,
 	KC_GRV, LCTL_T(KC_A),LALT_T(KC_S),LGUI_T(KC_D),LSFT_T(KC_F),KC_G,       KC_H, RSFT_T(KC_J),RGUI_T(KC_K),LALT_T(KC_L),RCTL_T(KC_SCLN),KC_QUOT, 	            KC_ENT,
 	KC_LSFT,	KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,	KC_UP,		KC_DEL,
-	KC_LCTL,	KC_LALT,	MO(6),										    LT(2, KC_SPC), 						KC_RGUI,	MO(1),   	KC_LEFT,	KC_DOWN,    KC_RGHT),
+	KC_LCTL,	KC_LALT,	MO(6),										    LT(2, KC_SPC), 						MO(1),	MO(1),   	KC_LEFT,	KC_DOWN,    KC_RGHT),
 
 // layer 1 Mac fn
 [1] = LAYOUT(
